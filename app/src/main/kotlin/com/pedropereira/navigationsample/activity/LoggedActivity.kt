9d23a03dfe4996cde894d942b91042e1be266abb
Augusto Pereira <p.augusto.pereira@accenture.com>
2018-07-30 15:59:01 -0300

    Adiciona Layouts e navegacao
    - Inclui layouts de todos os fragments
    - Altera LoggedFragment por LoggedActivity
    - Inclui navigation graph do fluxo de login e cadastro

---
+++ app/src/main/kotlin/com/pedropereira/navigationsample/activity/LoggedActivity.kt
@@ -0,0 +1,13 @@
+package com.pedropereira.navigationsample.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.pedropereira.navigationsample.R
+
+class LoggedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged)
+    }
+}
